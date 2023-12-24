cfee22b3be76f4b96f296a44b2aa9db5ee924def
Kishorekrazyyy <141833512+Kishorekrazyyy@users.noreply.github.com>
2023-12-24 23:44:34 +0530

    Update kishore.py

--- kishore.py
+++ kishore.py
@@ -1,1 +1,8 @@
-whoami
+# This is a simple Python program that asks for the user's name and greets them.
+
+# Get user input
+name = input("What is your name? ")
+
+# Greet the user
+print(f"Hello, {name}! Welcome to the world of Python programming.")
+
