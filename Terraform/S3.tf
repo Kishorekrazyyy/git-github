dfcb35e3f73e3c2f0e9ebae6e78cbadb79018d4a
Kishorekrazyyy <141833512+Kishorekrazyyy@users.noreply.github.com>
2024-02-09 01:13:22 +0530

    Create S3.tf

---
+++ Terraform/S3.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-north-1"
+}
+resource "aws_s3_bucket"  "s3_bucket" {
+    bucket = "kishorekrazy-s3-demo-krazyyy"
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "mayday"
+  }
+}
